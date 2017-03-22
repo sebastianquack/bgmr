033a92caf3bd2b98c8ddd3d98542a36fe1a8990c
Sebastian Quack <sebastianquack@gmail.com>
2017-03-22 13:17:25 +0100

    add area model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,9 @@
 class Project < ActiveRecord::Base
+
+	translates :title, :description
+
+	has_many :project_areas
+  	accepts_nested_attributes_for :project_areas, :allow_destroy => true
+  	has_many :areas, :through => :project_areas
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322115906) do
+ActiveRecord::Schema.define(version: 20170322120804) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20170322115906) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
+  create_table "areas", force: :cascade do |t|
+    t.string   "title_de"
+    t.string   "title_en"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pages", force: :cascade do |t|
     t.boolean  "draft"
     t.string   "title_de"
@@ -53,6 +60,13 @@ ActiveRecord::Schema.define(version: 20170322115906) do
     t.text     "content_de"
     t.text     "content_en"
     t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "project_areas", force: :cascade do |t|
+    t.integer  "project_id"
+    t.integer  "area_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
