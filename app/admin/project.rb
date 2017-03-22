e9f83d6ec902251b758ddd37f89ffccda12098ce
Sebastian Quack <sebastianquack@gmail.com>
2017-03-22 13:26:05 +0100

    enable check boxes for areas

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Project do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :title_de, :title_en, :description_de, :description_en, :slug, :draft
+permit_params :title_de, :title_en, :description_de, :description_en, :slug, :draft, area_ids:[]
 #
 # or
 #
@@ -21,6 +21,11 @@ form do |f|
     f.input :description_de, :input_html => { :class => 'ckeditor' }
     f.input :description_en, :input_html => { :class => 'ckeditor' }
   end
+
+  f.inputs "Arbeitsgebiete" do
+    f.input :areas, :label => false, :as => :check_boxes
+  end
+  
   f.inputs "Spezial" do
   	f.input :slug
   	f.input :draft
