fa9e7a63cf9b2d9e63513cddbfe414844bf67796
Sebastian Quack <sebastianquack@gmail.com>
2017-07-18 18:03:23 +0200

    set default value of slide link position to center

--- app/models/slide_link.rb
+++ app/models/slide_link.rb
@@ -1,4 +1,11 @@
 class SlideLink < ActiveRecord::Base
 	belongs_to :from_slide, :class_name => "Slide"
 	belongs_to :to_slide, :class_name => "Slide"
+
+	after_initialize :init
+
+    def init
+      self.pos_x  ||= 50           #will set the default value only if it's nil
+      self.pos_y  ||= 50
+    end
 end
