2e60e193bbb9f8203de30065f27e8d76b92e7ca6
Sebastian Quack <sebastianquack@gmail.com>
2017-07-24 12:35:38 +0200

    add radius to topic positioning

--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -39,63 +39,97 @@ function placeTopics() {
   var container_height = container_elem.offsetHeight;
 
   var elements = $(container_elem).find('.topic_container')
-
-  var number_of_elements = elements.length;
-  var elem_radius = $(elements).get(0).offsetWidth;
-  var overlap_radius = elem_radius * 0.62;
-
-  var positions = getRandomPositions(container_width, container_height, elem_radius, overlap_radius, number_of_elements)
+  
+  var positions = getRandomPositions(container_width, container_height, elements)
 
   for (var i = positions.length-1; i>=0; i--) {
     var e = $(elements).get(i)
     $(e).css('left',positions[i].x + "px")
     $(e).css('top',positions[i].y + "px")
+    //TODO: set size for element
+    //$(e).css('width', getRadius($(e).outerWidth(), $(e).data("projects")));
     $(e).addClass('positioned')
   }
 }
 
-function getRandomPositions(container_width, container_height, elem_radius, overlap_radius, number_of_elements) {
-  //console.log(container_width, container_height, elem_radius, number_of_elements)
-  var max_i = 100; // maxumum iterations (very simple non-deterministic algorithm)
+// calculates the radius from the css width and the number of projects for this topic
+function getRadius(width, projects) {
+  var weight = 1;
+  if(projects > 5) {
+    weight = 1.5;
+  }
+  if(projects > 10) {
+    weight = 2;
+  }
+  if(projects > 15) {
+    weight = 2.5;
+  }
+  return width * weight;
+}
 
+// generates a random position for an element
+function getRandomPosition(element, container_width, container_height) {
+
+  var elem_radius = getRadius($(element).outerWidth(), $(element).data("projects"));
+  
   var min_x = 0.5 * elem_radius
   var max_x = container_width - 0.5 * elem_radius;
   var min_y = 0.5 * elem_radius
   var max_y = container_height - 0.5 * elem_radius;  
-  var min_distance = overlap_radius;
-  var positions = []; // final positions go here
-  for (i = 0; i < max_i; i++) {
+  return {
+    x: Math.round(Math.random() * (max_x-min_x) + min_x ),
+    y: Math.round(Math.random() * (max_y-min_y) + min_y )
+  }
+}
+
+// checks if a position overlaps too much with existing already positioned elements
+function checkPosition(element, newPos, positions, elements) {
+
+  if (positions.length == 0) {
+    return true;
+  }
+  for (var i = positions.length-1; i >= 0; i--) {
+    var other_x = positions[i].x
+    var other_y = positions[i].y
+    var dist = Math.hypot(other_x - newPos.x, other_y - newPos.y)
+    //console.log(j, "dist: " + dist)
+
+    //get min distance for each element with radius
+    var min_distance = getRadius($(elements[i]).outerWidth(), $(elements[i]).data("projects")) * 0.62;
+
+    if (dist < min_distance) { // does not fit
+      //console.log("didn't fit")
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function getRandomPositions(container_width, container_height, elements) {
+  
+  var positions = []; // final positions go here - index corresponds to index of elements
+  for (var i = 0; i < elements.length; i++) {
     //console.log(positions)
-    // all done?
-    if (positions.length >= number_of_elements) {
-      break;  
+    
+    var max_j = 100; // maxumum iterations (very simple non-deterministic algorithm)
+    for(var j = 0; j < max_j; j++) {
+      
+      // 1. get new random position
+      var pos = getRandomPosition(elements[i], container_width, container_height);
+    
+      // 2. check if this positions is possible
+      if(checkPosition(elements[i], pos, positions, elements)) {
+        positions.push(pos);
+        break;
+      }       
     }
-    // 1. get random positions
-    var x = Math.round(Math.random() * (max_x-min_x) + min_x );
-    var y = Math.round(Math.random() * (max_y-min_y) + min_y );
-    // 2. check if this positions os possible
-    if (positions.length == 0) {
-      positions.push({ x:x, y:y })
-      continue;
+    // catch case where no position was ok
+    if(j >= max_j) {
+      console.log("canvas was too full, placing anyway");
+      positions.push(pos);
     }
-    for (j = positions.length-1; j >= 0 ; j--) {
-      var other_x = positions[j].x
-      var other_y = positions[j].y
-      var dist = Math.hypot(other_x-x, other_y-y)
-      //console.log(j, "dist: " + dist)
-      if (dist < min_distance) { // does not fit
-        //console.log("didn't fit")
-        break; 
-      }
-    }
-
-    if (j == -1) { // did fit
-      //console.log("adding "+x+","+y)
-      positions.push({ x:x, y:y })
-    }
-
-    //console.log(j)
-
+      
   }
   //console.log("finished after " + i + " iterations")
   return positions
