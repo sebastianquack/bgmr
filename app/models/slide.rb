b6ca8dbb5dd04ddfc958275e01e6a5f96e40c79b
Holger H <holgheiss@gmail.com>
2018-02-22 20:21:38 +0100

    plan grau weg

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,15 +1,26 @@
 class Slide < ActiveRecord::Base
 
 	has_attached_file :image, 
-		 styles: { large: "800x800>", zoom: "5501x5501>", plan_w2000: "2000x2000>", plan_w1000: "1000x1000>", plan_w500: "500x500>", w2000: "2000x2000>", w1000: "1000x1000>", w500: "500x500>", medium: "600x600>", thumb: "100x100>" }, default_url: "/assets/:style/missing.png",
+		 styles: { 
+		 	large: "800x800>", 
+		 	zoom: "5501x5501>", 
+		 	#plan_w2000: "2000x2000>", 
+		 	#plan_w1000: "1000x1000>", 
+		 	#plan_w500: "500x500>", 
+		 	w2000: "2000x2000>", 
+		 	w1000: "1000x1000>", 
+		 	w500: "500x500>", 
+		 	medium: "600x600>", 
+		 	thumb: "100x100>" 
+		 }, default_url: "/assets/:style/missing.png",
 		 :convert_options => {
 			:large      => "-quality 90",
 			:w2000      => "-quality 90",
 			:w1000      => "-quality 90",
 			:w500       => "-quality 95",
-			:plan_w2000 => "-quality 90 -strip -define modulate:colorspace=HSB -modulate 95",
-			:plan_w1000 => "-quality 90 -strip -define modulate:colorspace=HSB -modulate 95",
-			:plan_w500  => "-quality 95 -strip -define modulate:colorspace=HSB -modulate 95",
+			#:plan_w2000 => "-quality 90 -strip -define modulate:colorspace=HSB -modulate 95",
+			#:plan_w1000 => "-quality 90 -strip -define modulate:colorspace=HSB -modulate 95",
+			#:plan_w500  => "-quality 95 -strip -define modulate:colorspace=HSB -modulate 95",
 			:zoom       => "-quality 85 -strip -define modulate:colorspace=HSB -interlace Plane -background transparent -modulate 95"
 		}
 
